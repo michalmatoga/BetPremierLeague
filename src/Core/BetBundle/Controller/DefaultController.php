42cf8be45678a84838b6d231efe628b94f3ffc29
Michał Matoga <michalmatoga@gmail.com>
2012-12-10 20:12:48 +0100

    App demo skeleton

--- src/Core/BetBundle/Controller/DefaultController.php
+++ src/Core/BetBundle/Controller/DefaultController.php
@@ -9,10 +9,30 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      * @Template()
      */
     public function indexAction()
+    {
+
+      return array();
+    }
+
+    /**
+     * @Route("/demo", name="demo")
+     * @Template()
+     */
+    public function demoAction()
+    {
+
+      return array();
+    }
+
+    /**
+     * @Route("/about", name="about")
+     * @Template()
+     */
+    public function aboutAction()
     {
 
       return array();
